22fb94f364b25ed1e684b74efabfbeb36495a2a7
msabbagh <msabbagh@@tabadul.sa>
2024-06-04 18:10:19 +0300

    Add static login.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  username: any = "mohamed";
+  password: any = "12345";
   title = 'front-end';
+
+
+
+  ngOnInit(): void {
+    console.log('USERNAME = ', this.username);
+    console.log('PASSWORD = ', this.password);
+  }
+
+  submitLogin(username: any, password: any) {
+    console.log('USERNAME = ', username);
+    console.log('PASSWORD = ', password);
+  }
+
 }
